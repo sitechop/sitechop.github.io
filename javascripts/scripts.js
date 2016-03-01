ab28f08545632a2a84dc96aeaa44ed49b4b0af59
Robert Klein <itsrobbyk@gmail.com>
2016-03-01 09:55:08 -0800

    changes

--- javascripts/scripts.js
+++ javascripts/scripts.js
@@ -2,73 +2,88 @@ var general = {
   get : function(query) {
     var results = document.querySelectorAll(query);
     if (results.length === 1) return results[0]
-      else return results
+    else return results
   },
+
   loadCss : function(href) {
     var el = document.createElement('link')
     el.setAttribute('rel', 'stylesheet')
     el.setAttribute('type', 'text/css')
     el.setAttribute('href', href)
     this.get('head').appendChild(el)
-  },
-  init : function() {
-    this.loadCss('https://cloud.typography.com/7205494/6536952/css/fonts.css')
   }
 }
 
 var ajaxChimp = {
-  get : function(query) {
-    var results = document.querySelectorAll(query);
-    if (results.length === 1) return results[0]
-      else return results
-  },
-
   actionSwapper : function(form) {
     var url = form.getAttribute('action')
     url = url.replace("post?u=", "post-json?u=")
     url += '&c=callback'
-    this.get('#mailing-list').setAttribute('action', url)
+    form.setAttribute('action', url)
+  },
+
+  sendingState : function(submit) {
+    submit.value = 'Sending'
+    submit.disabled = true;
+  },
+
+  reset : function(submit) {
+    submit.value = 'Submit'
+    submit.disabled = false;
   },
 
   submit : function(form) {
-    var email = this.get('#mailing-list-email').value
-    var submit = this.get('#mailing-list-submit')
+    var email = form.querySelectorAll('[type="email"]')[0],
+      submit = form.querySelectorAll('[type="submit"]')[0],
+      responseEl = form.querySelectorAll('.response')[0]
     
-    submit.value = 'Sending'
+
+    this.sendingState(submit)
 
     url = form.getAttribute('action')
-    data = '&EMAIL=' + encodeURIComponent(email)
-    
+    url += '&EMAIL=' + encodeURIComponent(email.value)
+
     window.callback = function(data) {
-      ajaxChimp.mailchimpResponse(data.msg)
-      ajaxChimp.get('body').removeChild(scriptEl);
-      submit.value = 'Submit'
+      ajaxChimp.mailchimpResponse(data.msg, responseEl)
+      general.get('body').removeChild(scriptEl);
+      ajaxChimp.reset(submit)
     };
 
     var scriptEl = document.createElement('script');
-    scriptEl.setAttribute('src', url + data);
+    scriptEl.setAttribute('src', url);
     document.body.appendChild(scriptEl);
   },
 
-  mailchimpResponse : function(data) {
-    var responseEl = this.get('#mailing-list-response')
+  mailchimpResponse : function(data, responseEl) {
     var dataSplit = data.split(' - ') 
-
     if (dataSplit.length == 1) response = data
       else response = dataSplit[1]
 
     responseEl.innerHTML = response
   },
 
-  init : function() {
-    mailchimpForm = this.get('#mailing-list') 
-    this.actionSwapper(mailchimpForm)
-    mailchimpForm.addEventListener("submit", function(e) {
+  init : function(form) {
+    this.actionSwapper(form)
+
+    form.addEventListener("submit", function(e) {
       e.preventDefault()
       ajaxChimp.submit(e.target)
     })
   }
 }
 
-general.init()
-ajaxChimp.init()
+var sitechop = {
+  init : function() {
+    var mailingLists = document.querySelectorAll('.mailing-list')
+
+    general.loadCss('https://cloud.typography.com/7205494/6536952/css/fonts.css')
+    
+    for (var i=0; i<mailingLists.length;i++)
+      ajaxChimp.init(mailingLists[i])
+  }
+}
+
+sitechop.init()
+
+
+  
